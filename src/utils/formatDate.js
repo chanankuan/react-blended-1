fb1583d4b4cbb6c2c378206ed764894794c0e4ba
dn280786lea <ekaterina.lubenets@gmail.com>
2023-11-05 15:28:02 +0100

    task4
    task4

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,9 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
 
 export const formatDateToNow = date => {
   return formatDistanceToNow(new Date(date), { addSuffix: true });
+};
+
+export const formateDate = date => {
+  return format(new Date(date), 'Pp');
 };
